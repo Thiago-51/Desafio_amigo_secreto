96d15d1f92a9d51a49b2dd13ff230dfc2d47314e
Thiago Barbosa do Carmo <barbosadocarmothiago@gmail.com>
2025-03-03 13:16:07 -0300

    refazendo a função de sorteio

--- app.js
+++ app.js
@@ -16,3 +16,12 @@ function limparCampo() {
     amigo.value = '';
 }
 
+function sorteio() {
+    let quantidadeAmigos = listaAmigos.length;
+    if (quantidadeAmigos == 0) {
+        console.log("Não há amigos para realizarmos o sorteio.");
+    } else {
+        console.log("tudo certo")
+    }
+    
+}
